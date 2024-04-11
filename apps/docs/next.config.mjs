08a4c0ea1980a55ab857a278674981569db1b30e
cieloazul310 <cieloazul310@gmail.com>
2024-04-11 11:41:49 +0900

    chore: update

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -27,6 +27,7 @@ const nextConfig = {
     unoptimized: true,
   },
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  transpilePackages: ["@cieloazul310/regista"],
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
